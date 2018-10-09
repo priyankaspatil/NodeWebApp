b72c3cd714ebeb45c7527a56fad32c489934f67c
nikita <nikitashivangirikar@gmail.com>
2018-10-09 05:04:39 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express= require('express')
+const fs=require('fs')
+
+var app=express();
+
+app.set('view engine','ejs')
+
+app.use(express.static(__dirname + '/public'));
+
+/* app.use((req,res,next)=>{
+    res.render('maintainance.ejs');
+    
+}); */
+
+app.use((req,res,next)=>{
+    var logg= new Date().toString();
+    logg =logg + req.method + req.path;
+    fs.appendFile('server.log',logg)
+    next();
+});
+
+app.get('/',(req,res)=>{
+    res.send('<h1>Hello Express</h1>');
+});
+
+
+app.get('/user',(req,res)=>{
+  res.render('home.ejs',{
+      pagetitle:"Home Page",
+      username:"Nikita"
+  });
+  
+  
+  
+    /*  res.send({
+        name:'FIGmd',
+        address:'Baner'
+    }); */
+});
+
+app.listen(3000,()=>{
+    console.log('server started  at localhost:3000')
+});
